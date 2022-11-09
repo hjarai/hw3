2eb937d44ae6b075ee9381722b0a42b58d24ce70
hjarai <harai@middlebury.edu>
2022-11-09 10:40:51 -0800

    first commit

---
+++ preparedata.py
@@ -0,0 +1,22 @@
+"""
+converts CoNLL data (train and dev) into features of the parser configuration paired
+with parser decisions. This should be human-readable, i.e. a text file of words/labels. The format
+should be described in the README
+"""
+#%%
+file = "train.orig.conll"
+
+with open(file, 'r', encoding='utf-8') as file:
+    sentence_cache
+    for line in file:
+        if
+    hi = [line.split() for line in file]
+    
+       
+# %%
+
+# class token():
+
+#initialized with a list of token instances
+# class sentence():
+
